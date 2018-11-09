1f2e78b2a645c03528b04420a0f2d5de3ce24294
Kirk Daniels <kirk@lab.co.uk>
2018-11-09 14:25:18 +0200

    sticky header

--- dist/js/app.js
+++ dist/js/app.js
@@ -191,6 +191,23 @@ $('.o-hamburger').click(function(e) {
     
 // A.3. SCROLL TO LINK ------------------------------------------------------------------------------------------------
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("o-header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 
 // A.3. END -----------------------------------------------------------------------------------------------------------
     
